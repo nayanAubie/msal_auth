e0d9990fb99136b2a5593b83ead9f396bf726fed
eugeniosoddu <eugenio.soddu@gmail.com>
2025-07-23 13:25:23 +0200

    BUG FIX #118: iOS authorityType b2c configuration Open issue: https://github.com/nayanAubie/msal_auth/issues/118

--- ios/Classes/MsalAuthPlugin.swift
+++ ios/Classes/MsalAuthPlugin.swift
@@ -30,7 +30,7 @@ public class MsalAuthPlugin: NSObject, FlutterPlugin {
                 let clientId = dict["clientId"] as? String,
                 let broker = dict["broker"] as? String,
                 let authorityType: AuthorityType = {
-                    switch call.method {
+                    switch dict["authorityType"] as? String {
                     case "b2c": return .b2c
                     default: return .aad
                     }
